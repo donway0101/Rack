305a8489b0ed71dd508720ff79b41889437ef186
donway0101 <donway0101@gmail.com>
2019-03-06 12:00:57 +0800

    build Motion control structure

---
+++ Motion/Motor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACS.SPiiPlusNET;
+
+namespace Motion
+{
+    public class Motor
+    {
+        public Axis Id { get; set; }
+
+        /// <summary>
+        /// Encoder counts per round.
+        /// </summary>
+        public double EncCtsPerR { get; set; }
+
+        public double BallScrewLead { get; set; }
+
+        public double EncoderFactor { get; set; }
+
+        public double HomeOffset { get; set; }
+
+        /// <summary>
+        /// Critical error on acceleration.
+        /// </summary>
+        public double CriticalErrAcc { get; set; }
+
+        public double CriticalErrVel { get; set; }
+
+        public double CriticalErrIdle { get; set; }
+
+        public double SoftLimitNagtive { get; set; }
+
+        public double SoftLimitPositive { get; set; }
+
+        public double MaxTravel { get; set; }
+
+        public int ErrCode { get; set; }
+
+        public Motor()
+        {
+
+        }
+
+
+    }
+}
