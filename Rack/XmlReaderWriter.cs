d5e16d0f5aff229126ae43dad587eac1cd5b1b46
donway0101 <donway0101@gmail.com>
2019-03-19 19:14:56 +0800

    Add Xml read and write

---
+++ Rack/XmlReaderWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Rack
+{
+    public class XmlReaderWriter
+    {
+        private XmlReader Reader { get; set; }
+
+        public static IEnumerable<XElement> SimpleStreamAxis(string inputUrl,
+                                              string elementName)
+        {
+            using (XmlReader reader = XmlReader.Create(inputUrl))
+            {
+                reader.MoveToContent();
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name == elementName)
+                        {
+                            XElement el = XNode.ReadFrom(reader) as XElement;
+                            if (el != null)
+                            {
+                                yield return el;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Save(string file)
+        {
+            XElement root;
+
+            if (File.Exists(file))
+                root = XElement.Load(file);
+            else
+                root = new XElement("Devices");
+
+            XElement bookParticipants =
+                new XElement("Boxs",
+                new XElement("Box",
+                new XAttribute("Id", "1"),
+                new XAttribute("CarrierHeight", "600"),
+                new XAttribute("DoorHeight", "750"),
+                new XAttribute("XPosition", "100"),
+                new XAttribute("YPosition", "300"),
+                new XElement("FirstName", "J"),
+                new XElement("LastName", "Ri")));
+
+            
+
+            root.Add(bookParticipants);
+            root.Save(file);
+        }
+    }
+}
---
+++ RackTool/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Rack;
+
+namespace RackTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IEnumerable<XElement> xElements = XmlReaderWriter.SimpleStreamAxis("book.xml", "Box");
+                ;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+    }
+}
