0cd859dd38ce2909e65383796ce05d713d2bc050
donway0101 <donway0101@gmail.com>
2019-04-12 18:23:47 +0800

    Testing function

---
+++ RackFunctionTester/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rack
+
+namespace RackFunctionTester
+{
+    public partial class RackFunctionTester : Form
+    {
+        public RackFunctionTester()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
